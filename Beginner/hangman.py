4ccedc42eeb227cc048222c1807f85b3b24c3fca
Manvikri22 <manvikri2@gmail.com>
2021-06-18 18:15:43 +0530

    intial commit

---
+++ Beginner/hangman.py
@@ -0,0 +1,12 @@
+import random
+from words import words
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-'in word or ' ' in word:
+        word = random.choice(words)
+    return word
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters =
